7fa216d73c8425b72a3189829abf0f32e43a47ac
Thiago-Diniz <74604325+Thiago-Dinix@users.noreply.github.com>
2021-05-10 20:39:06 -0300

    Upadate do script
    Adicionado a linha 18, e corrigidos os erros 16;6

--- script.js
+++ script.js
@@ -13,4 +13,6 @@ let string1 = nome
 
 console.log(`O nome dele é ${nome}, ele nasceu em ${anoNascimento}, e em 2021 ele vai fazer ou já tem ${number1-number2} anos!`)
 
-alert(`O seu nome é ${nome}, ele nasceu em ${anoNascimento}, e em 2021 ele vai fazer ou já fez ${number1-number2} anos!`)
+alert(`O seu nome é ${nome}, e você nasceu em ${anoNascimento}, e em 2021 ele vai fazer ou já fez ${number1-number2} anos!`)
+
+const certo = prompt("Estou certo?")
